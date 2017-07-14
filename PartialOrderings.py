a991341dd83255091c66b26786ae77b7c05c9f4d
Shyamal Patel <sbptl2@gmail.com>
2017-07-14 14:22:43 -0400

    First commit

---
+++ PartialOrderings.py
@@ -0,0 +1,32 @@
+def partialOrderings(string, solved):
+    count = 0
+    solved[string] = []
+    if len(string) == 1:
+        solved[string] = 1
+        return
+    if string[0] == "D":
+        partial = string[1:]
+        if (partial not in solved):
+            partialOrderings(partial, solved)
+        count += solved[partial]
+    if string[len(string) - 1] == "U":
+        partial = string[:len(string) - 1]
+        if (partial not in solved):
+            partialOrderings(partial, solved)
+        count += solved[partial]
+    for i in range(0, len(string) - 1):
+        if string[i] == "U":
+            if string[i + 1] == "D":
+                partial1 = string[:i] + "U" + string[i + 2:]
+                partial2 = string[:i] + "D" + string[i + 2:]
+                if partial1 not in solved:
+                    partialOrderings(partial1, solved)
+                count += solved[partial1]
+                if partial2 not in solved:
+                    partialOrderings(partial2, solved)
+                count += solved[partial2]
+                i += 1
+    solved[string] = count
+    return count
+
+print(partialOrderings("DUUDUDU", {}))
